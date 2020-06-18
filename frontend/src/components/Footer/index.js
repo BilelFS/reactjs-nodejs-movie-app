b8384ee0dde831ce85df074a0fde0e954a78748f
BilelFS <fs.bilel@gmail.com>
2020-06-18 14:32:47 +0200

    commit all the project

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Footer = () => {
+  return <div className="w-100">
+      <p style={{background: '#292a2b', fontSize: '20px', textAlign: "center"}} className="text-white py-4 m-0">made by Fatnassi Bilel</p>
+    </div>;
+}
+export default Footer;
---
+++ frontend/src/components/common/Select/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+const Select = ({ name, label,value, options, error, iconClass }) => {
+  if(!options) return <> </>;
+  
+  return ( 
+    <div className="input-container">
+      {label && <label htmlFor={name}> {label} </label> }
+      <div className={`input-icon ${iconClass}`}/> 
+      <select name={name}>
+        <option disabled value="">Select</option>
+        {options.map(element => (
+          <option key={element._id} value={element.genre} >
+            {element.genre} 
+          </option>
+        ))}
+      </select>
+      {error && <div className="alert alert-danger"> {error} </div>}
+    </div>
+  );
+}
+ 
+export default Select;
